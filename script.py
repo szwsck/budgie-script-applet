4cb4a2ceb86cbc61a1f0120cfae205309bc44f3e
Szymon Wysocki <sz.wsck@gmail.com>
2020-12-23 19:37:54 +0100

    initial commit

---
+++ script.py
@@ -0,0 +1,73 @@
+import gi.repository
+import subprocess
+from settings import ScriptSettings
+
+gi.require_version("Budgie", "1.0")
+gi.require_version("Gtk", "3.0")
+from gi.repository import Budgie, GObject, Gtk  # noqa: E402
+
+
+class BudgieScript(GObject.GObject, Budgie.Plugin):
+
+    # set unique id for this plugin
+    __gtype_name__ = "BudgieScript"
+
+    def do_get_panel_widget(self, uuid):
+        return BudgieScriptApplet(uuid)  # an instance of Budgie.Applet
+
+
+class BudgieScriptApplet(Budgie.Applet):
+
+    def __init__(self, uuid):
+        Budgie.Applet.__init__(self)
+
+        self.uuid = uuid
+        self.job_id = None
+
+        # widget UI
+        self.button = Gtk.Button.new()
+        self.button.set_relief(Gtk.ReliefStyle.NONE)
+        self.add(self.button)
+        self.show_all()
+
+        self.settings = ScriptSettings(uuid, on_changed=self.schedule)
+
+        # schedule execution
+        self.schedule()
+
+    def schedule(self):
+
+        # cancel currrent job
+        if self.job_id:
+            GObject.source_remove(self.job_id)
+
+        # run once on start
+        self.update()
+
+        # schedule to run every <interval> seconds
+        interval = self.settings.load()["interval"]
+        self.job_id = GObject.timeout_add_seconds(interval, self.update)
+
+    def update(self):
+
+        # load command for this script from settings
+        command = self.settings.load()["command"]
+        stdout = self.run(command)
+
+        self.button.set_label(stdout)
+        return True
+
+    def run(self, command):
+
+        # run given command with shell
+        result = subprocess.run(command,
+                                shell=True,
+                                stdout=subprocess.PIPE,
+                                text=True)
+        return result.stdout.strip()
+
+    def do_supports_settings(self):
+        return True
+
+    def do_get_settings_ui(self):
+        return self.settings.get_ui()
---
+++ settings.py
@@ -0,0 +1,140 @@
+import gi.repository
+
+from appdirs import AppDirs
+from pathlib import Path
+import os
+import json
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk  # noqa: E402
+
+_appdirs = AppDirs(appname="budgie_script", appauthor="wsck")
+
+_directory = _appdirs.user_config_dir
+_filename = "budgie_scripts.json"
+_path = os.path.join(_directory, _filename)
+
+_default = {"command": "date +%H:%M:%S", "interval": 1}
+
+
+class ScriptSettings:
+
+    def __init__(self, uuid, on_changed):
+        self._uuid = uuid
+        self._callback = on_changed
+        self._create_ui()
+
+    # =====================================
+    # Saved settings manipulation
+
+    def _load_all(self):
+
+        if not os.path.exists(_path):
+            print("Settings file doesn't exist")
+            return {}
+
+        with open(_path, "r") as file:
+            all = json.load(file)
+
+        return all
+
+    def load(self):
+
+        all = self._load_all()
+        return all.get(self._uuid, _default)
+
+    def save(self, box):
+
+        new = {
+            "command": self.cmd_input.get_text(),
+            "interval": self.interval_input.get_value_as_int()
+        }
+
+        all = self._load_all()
+        all[self._uuid] = new
+
+        Path(_directory).mkdir(parents=True, exist_ok=True)
+
+        with open(_path, "w") as file:
+            json.dump(all, file)
+
+        self._callback()
+
+    # =====================================
+    # UI methods
+
+    def get_ui(self):
+        return self.settings_ui
+
+    def _create_ui(self):
+
+        # This should be done using glade, @TODO
+
+        current = self.load()
+
+        # command
+
+        label1 = Gtk.Label(
+            label="Command",
+            can_focus=False,
+            halign=Gtk.Align.START,
+            hexpand=True
+        )
+        self.cmd_input = Gtk.Entry(
+            text=current['command'],
+            can_focus=True,
+            halign=Gtk.Align.END
+        )
+
+        option1 = Gtk.Box(spacing=6)
+        option1.add(label1)
+        option1.add(self.cmd_input)
+
+        # interval
+        label2 = Gtk.Label(
+            label="Interval",
+            can_focus=False,
+            halign=Gtk.Align.START,
+            hexpand=True
+        )
+        adjustment = Gtk.Adjustment(
+            lower=1,
+            upper=3600,
+            step_increment=1,
+            page_increment=60,
+            value=current["interval"],
+        )
+        self.interval_input = Gtk.SpinButton(
+            numeric=True,
+            adjustment=adjustment,
+            can_focus=True,
+            halign=Gtk.Align.END
+        )
+
+        label3 = Gtk.Label(
+            label="seconds",
+            can_focus=False,
+            halign=Gtk.Align.END,
+            hexpand=True
+        )
+
+        option2 = Gtk.Box(spacing=6)
+        option2.add(label2)
+        option2.add(self.interval_input)
+        option2.add(label3)
+
+        # save button
+        save_button = Gtk.Button.new_with_label("Save")
+        save_button.connect("clicked", self.save)
+
+        self.settings_ui = Gtk.Box(
+            orientation=Gtk.Orientation.VERTICAL,
+            spacing=10
+        )
+        self.settings_ui.add(option1)
+        self.settings_ui.add(option2)
+        self.settings_ui.add(save_button)
+
+        option1.show_all()
+        option2.show_all()
+        self.settings_ui.show_all()
